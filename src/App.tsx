2626f7e97c5ee1998e5fc426c7e55b795388894a
BangZaky0029 <bangzaky0029@gmail.com>
2026-03-10 11:44:03 +0700

    udpate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { QRScanner } from './features/connector/QRScanner'
 import { DashboardStats } from './features/dashboard/DashboardStats'
